a98726e44682bff2e7b1f9ae87b679cbb384e950
jackkimmins <jackkimmins@outlook.com>
2023-04-19 12:06:52 +0000

    Added ExecuteStoredProcedure feature

--- JollyWrapper/QueryExecutions/StoredProcedures.cs
+++ JollyWrapper/QueryExecutions/StoredProcedures.cs
@@ -11,9 +11,54 @@ namespace JollyWrapper
         /// <param name="parameters"></param>
         /// <returns>QueryData Object</returns>
         /// <example>
-        public static async Task<QueryData> ExecuteProcedure(string query, QueryParms parameters = null)
+        public static async Task<QueryData> ExecuteProcedure(string storedProcedureName, QueryParms parameters = null)
         {
-            return await ExecuteQuery(query, parameters);
+            QueryData rows = new QueryData();
+            MySqlConnection connection = await OpenConnection();
+
+            try
+            {
+                using (MySqlCommand cmd = connection.CreateCommand())
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandText = storedProcedureName;
+
+                    if (parameters != null)
+                    {
+                        foreach (var parameter in parameters)
+                        {
+                            cmd.Parameters.AddWithValue(parameter.Key, (object)parameter.Value ?? DBNull.Value);
+                        }
+                    }
+
+                    await cmd.PrepareAsync();
+
+                    using (System.Data.Common.DbDataReader reader = await cmd.ExecuteReaderAsync())
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            Dictionary<string, string> row = new Dictionary<string, string>();
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                row.Add(reader.GetName(i), reader.GetString(i));
+                            }
+                            rows.Add(row);
+                        }
+
+                        reader.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                OutputError(ex.Message);
+            }
+            finally
+            {
+                await CloseConnection(connection);
+            }
+
+            return rows;
         }
     }
 }
